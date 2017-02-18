c4fa7346326500428aa2f40a8297088674735ad4
liaklein <lklein13@u.rochester.edu>
2017-02-18 01:18:25 -0500

    trying again

--- testing.py
+++ testing.py
@@ -2,11 +2,15 @@ from __future__ import print_function
 import requests
 import json
 #from requests_oauthlib import OAuth1
-
-url = "https://api.kairos.com/recognize"
-#auth = OAuth1('6c53b4533d32d3773539951326ea7118')
-
-r = requests.post(url, json="request.json")
-
+url = "https://api.kairos.com/enroll"
+headers = {'Content-Type' : 'application/json','app_id' : 'f62afb61','app_key':'6c53b4533d32d3773539951326ea7118'}
+r = requests.post(url,json="enroll.json",headers = headers)
 print(r.status_code, r.reason)
 print(r.text[:500])
+
+#url = "https://api.kairos.com/recognize"
+#auth = OAuth1('6c53b4533d32d3773539951326ea7118')
+#r = requests.post(url, json="request.json")
+
+#print(r.status_code, r.reason)
+#print(r.text[:500])
