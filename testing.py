1e969a5dce12e09254d40a8a5e709f7ca99f3629
Jacob Bisnett <jacob.bisnett@gmail.com>
2017-02-18 01:29:50 -0500

    kairos hello world

--- testing.py
+++ testing.py
@@ -4,7 +4,12 @@ import json
 #from requests_oauthlib import OAuth1
 url = "https://api.kairos.com/enroll"
 headers = {'Content-Type' : 'application/json','app_id' : 'f62afb61','app_key':'6c53b4533d32d3773539951326ea7118'}
-r = requests.post(url,json="enroll.json",headers = headers)
+json = {
+    "image":" http://media.kairos.com/kairos-elizabeth.jpg ",
+    "subject_id":"Elizabeth",
+    "gallery_name":"MyGallery"
+}
+r = requests.post(url, json=json, headers = headers)
 print(r.status_code, r.reason)
 print(r.text[:500])
 
